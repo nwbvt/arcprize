6846a51effff1190b079c7885ff87077b846cdfb
Nicholas Brown <nwbrown@gmail.com>
2025-05-15 20:35:31 -0400

    Add attention mechanism

--- solver.py
+++ solver.py
@@ -10,14 +10,18 @@ class SolvingModel(nn.Module):
     def __init__(self, encoder, size, kernel, dropout):
         super().__init__()
         self.encoder = encoder
-        self.conv = nn.Conv2d(encoder.out_size, size, kernel, padding="same")
+        encoded_size = encoder.out_size+data.NUM_VALUES+1
+        self.attn = nn.Linear(size, encoded_size)
+        self.conv = nn.Conv2d(encoded_size, size, kernel, padding="same")
         self.final = nn.Linear(size, data.NUM_VALUES+1)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-        encoded = self.encoder(x)
+        encoded = torch.cat([x, self.encoder(x)], dim=1)
         hidden = F.relu(self.dropout(self.conv(encoded)))
-        y = self.final(hidden.permute([0,2,3,1]))
+        attention = F.softmax(torch.matmul(encoded.permute((0,2,3,1)), self.attn.weight), dim=2)
+        selected = torch.mul(hidden.permute((0,2,3,1)), attention)
+        y = self.final(selected)
         return y.permute([0,3,1,2])
 
 def train_mini_model(train_inputs, train_outputs, model, n_epochs=10, lr=0.001, l2=0.01):
